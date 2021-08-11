c67f73b45df0a5ae1abda3ef2df0e7a88e8e9c4b
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-08-11 08:31:11 -0300

    feature:Buscas por produtos e testes rodando

--- searches/services.py
+++ searches/services.py
@@ -1,13 +1,15 @@
 from rest_framework import status
 from services.models import Service
+from products.models import Product
 from services.serializers import ServiceSerilizer
+from products.serializers import ProductSerializer
 from rest_framework.response import Response
 
 
 def createServices(data):
     search_ = dict
     services = []
-    print(data)
+ 
     if data['city']:
         search_ = Service.objects.filter(city = data['city'])
     elif data['state']:
@@ -23,4 +25,16 @@ def createServices(data):
 
 
 def createProducts(data):
-    pass
+    product_ = dict
+    productes = []
+   
+    if data['type']:
+        product_ = Product.objects.filter(type = data['type'])
+    elif data['category']:
+        product_ = Product.objects.filter(category = data['category'])
+
+    for data in product_:
+            product = ProductSerializer(data).data
+            productes.append(product)
+    return Response(productes,
+                        status=status.HTTP_200_OK) 
