98324195dfec29aa21989cad388b47c63dc25567
Larissa <larissa.sena@mail.com>
2021-09-04 21:05:06 -0300

    feature: criação da classe database e ajustes na app attendance

--- attendance/api.py
+++ attendance/api.py
@@ -5,6 +5,5 @@ from attendance.services import create
 class Atendimento(APIView):
 
     def post(self, request):
+        print("request: ", request)
         return create(request.data)
-
-
--- classes/attendance.py
+++ classes/attendance.py
@@ -1,6 +1,7 @@
+from classes.database import DataBase
 
 
-class Attendance():
+class Attendance(DataBase):
 
     def _init_(self):
 
@@ -13,34 +14,14 @@ class Attendance():
         self.city: str = ""
         self.state: str = ""
         self.phone: str = ""
-
         self.product_name: str = ""
         self.product_category: str = ""
         self.product_value: float = 0.0
         self.product_type: str = ""
-
         self.service_name: str = ""
         self.service_category: str = ""
         self.service_date_time: str = ""
         self.service_value: float = 0.0
-
-    # TODO: TUDO EM INGLÊS
-    # TODO: criar outro app pra gerir o atendimento (não tem serializer nem model)
-    # Nome pessoa: name
-    # cpf pessoa : cpf
-    # logradouro: place
-    # número: number
-    # rua: street
-    # bairro: (block?)
-    # cidade: city
-    # estado: state
-    # telefone: phone
-    # produto: product
-    # quantidade de produto: product_quantity
-    # nome serviço: service_name
-    # data / horas de agendamento do serviço: date_time
-    # valor do serviço: service_price
-    # valor do produto por unidade: product_unit_price
 
     def data_receive(self, data: dict) -> bool:
 
@@ -53,14 +34,47 @@ class Attendance():
         self.city = data.get("city")
         self.state = data.get("state")
         self.phone = data.get("phone")
-
         self.product_name = data.get("product_name")
         self.product_category = data.get("product_category")
         self.product_value = data.get("product_value")
         self.product_type = data.get("product_type")
-        
         self.service_name = data.get("service_name")
         self.service_category = data.get("service_category")
         self.service_value = data.get("service_value")
 
-        # self.name = data["name"]
+    def saveProducts2(self):
+
+        data = {
+            'name': self.product_name,
+            'category': self.product_category,
+            'value': self.product_value,
+            'type': self.product_type,
+        }
+
+        self.saveProduct(data)
+
+    def saveServices2(self):
+
+        data = {
+            'name': self.service_name,
+            'category': self.service_category,
+            'value': self.service_value,
+        }
+
+        self.saveService(data)
+    
+    def savePersonalData2(self):
+        
+        data = {
+            'name': self.name,
+            'cpf': self.cpf,
+            'place': self.place,
+            'number': self.number,
+            'street': self.street,
+            'district': self.district,
+            'city': self.city,
+            'state': self.state,
+            'phone': self.phone
+        }
+
+        self.savePersonalData(data)
