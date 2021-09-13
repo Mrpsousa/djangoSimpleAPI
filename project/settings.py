c38708da87c6b6b1ac79118dd5d62d1128c22ce1
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-09-12 21:30:33 -0300

    feature: modificaçoes com docker, conexao com banco de dados, em teste

--- project/settings.py
+++ project/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'testedb',
+        'USER': 'root',
+        'PASSWORD': 'MySql2019!',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
